b38964dc1b5be1fe4e03114861fa5c6eeedcfcaf
Leon <>
2022-02-09 16:02:50 +0200

    first commit

---
+++ traffic-viewer/src/global.d.ts
@@ -0,0 +1,8 @@
+declare module "*.svg" {
+    const content: any;
+    export default content;
+  }
+
+  declare module '*.scss';
+  declare module '*.sass';
+  declare module '*.png';
---
+++ traffic-viewer/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { TrafficPage } from './components/TrafficPage'
+
+export const TrafficViewer = ({ text }) => {
+  return <TrafficPage></TrafficPage>
+}
